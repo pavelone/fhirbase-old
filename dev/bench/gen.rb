513b367b32e5edcbbf3cf7867a9a49007f32d39d
Danil Kutkevich <danil@kutkevich.org>
2014-04-04 15:36:32 +0400

    benchmar: refactor json generator methods params

--- dev/bench/gen.rb
+++ dev/bench/gen.rb
@@ -1,39 +1,76 @@
 # require 'faker'
 
 def gen_identifier(number = 1)
+  n = if number.is_a?(Range)
+        number.last < Float::INFINITY ? number.last : 9
+      else
+        number
+      end
+
   tpls = [
     { use: 'official', label: 'BSN', system: 'urn:oid:2.16.840.1.113883.2.4.6.3', value: '123123' },
     { use: "official", label:"SSN", system:"urn:oid:2.16.840.1.113883.2.4.6.7", value:"123456789" }]
-  number.times.map do
+  n.times.map do
     tpls.sample.merge value: (1000000 + rand(1000000)).to_s, use: %w(usual  official  temp  secondary).sample
   end
 end
 
-def gen_category ; end
-def gen_name ; end
-def gen_telecom ; end
-def gen_gender ; end
-def gen_birthDate ; end
-def gen_deceasedBoolean ; end
-def gen_address ; end
-def gen_maritalStatus ; end
-def gen_photo ; end
-def gen_contact ; end
-def gen_communication ; end
-def gen_careProvider ; end
-def gen_managingOrganization ; end
+def gen_category(number = 1) end
+def gen_name(number = 1) end
+def gen_telecom(number = 1) end
+def gen_gender(number = 1) end
+def gen_birthDate(number = 1) end
+def gen_deceasedBoolean(number = 1) end
+def gen_address(number = 1) end
+def gen_maritalStatus(number = 1) end
+def gen_photo(number = 1) end
+def gen_contact(number = 1) end
+def gen_communication(number = 1) end
+def gen_careProvider(number = 1) end
+
+def gen_managingOrganization(number = 1) #0..1
+  res = {}
 
 
-def gen_active(number = 1)
+  res
+end
+
+def gen_active(number = 1) #0..1
   [true, false].sample
+end
+
+def gen_codeable_concept(number = 1)
+  res = {}
+  object :coding, (0..Float::INFINITY) do
+    # Coding Code defined by a terminology system
+  end
+  res = string :text, 0..1 # Plain text representation of the concept
+
+  res
 end
 
 def gen_patient(number)
   res = { resourceType: 'Patient' }
   #contained
 
-  %i(identifier category name telecom gender birthDate deceasedBoolean address maritalStatus photo contact communication careProvider managingOrganization active).each do |name|
-    res[name] = send("gen_#{name}")
+  {
+    identifier:           0..Float::INFINITY,
+    category:             0, #wtf?
+    name:                 0..Float::INFINITY,
+    telecom:              0..Float::INFINITY,
+    gender:               0..1,
+    birthDate:            0..1,
+    deceasedBoolean:      0..1,
+    address:              0..Float::INFINITY,
+    maritalStatus:        0..1,
+    photo:                0..Float::INFINITY,
+    contact:              0..Float::INFINITY,
+    communication:        0..Float::INFINITY,
+    careProvider:         0..Float::INFINITY,
+    managingOrganization: 0..1,
+    active:               0..1
+  }.each do |name, number|
+    res[name] = send("gen_#{name}", number)
   end
 
   res
